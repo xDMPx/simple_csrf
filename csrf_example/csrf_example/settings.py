1a047053b2ce36389bc1df19381d42184d4b77be
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-03-21 13:18:20 +0100

    Adjust session cookie configurations to permit CSRF vulnerability

--- csrf_example/csrf_example/settings.py
+++ csrf_example/csrf_example/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
